6392978e206482601823caee398c8e2f43a352ea
susanacst <susanacst098@gmail.com>
2025-01-27 20:49:41 -0600

    primeras clases alura curso clase 5

---
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -0,0 +1,14 @@
+alert('Hola de nuevo, mundo'); //función, lo que va dentro del paréntesis se llaman parámetros, el cual es un valor, en este caso texto.
+
+// prompt("Introduce un número, por favor:")
+
+// const, let y var
+//variables escritas en CamelCase
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un numero por favor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el numero');
+}
